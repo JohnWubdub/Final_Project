c905682484e83eabc189056f3f20eb3771b105a5
Tyler <tjt288@nyu.edu>
2018-03-26 00:36:18 -0400

    Fixed game, added some features

--- Assets/Scripts/MenuSelection.cs
+++ Assets/Scripts/MenuSelection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+//using System.Management.Instrumentation;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -20,6 +21,9 @@ public class MenuSelection : MonoBehaviour
 	public bool yes = false;
 	public bool no = false;
 	
+	private float timeAdd = 3f;
+	private bool swapped = false;
+	
 	void Start () 
 	{
 		
@@ -28,24 +32,43 @@ public class MenuSelection : MonoBehaviour
 
 	void Update () 
 	{
-		if (Input.GetKey(KeyCode.W) == true)
+		
+		if (Input.GetKey(KeyCode.W) == true && player.GetComponent<Timer>().timeUp == false)
 		{
 			option1.GetComponent<Renderer>().enabled = true;
 			option2.GetComponent<Renderer>().enabled = false;
-			Debug.Log("fuck me");
-			yes = true;
+//			Debug.Log("fuck me");
+			if (swapped == false)
+			{
+				yes = true;
+				no = false;
+			}
+			else
+			{
+				yes = false;
+				no = true;
+			}
 		}
 		
-		if (Input.GetKey(KeyCode.S) == true)
+		if (Input.GetKey(KeyCode.S) == true && player.GetComponent<Timer>().timeUp == false)
 		{
 			option1.GetComponent<Renderer>().enabled = false;
 			option2.GetComponent<Renderer>().enabled = true;
-			Debug.Log("fuck you");
-			no = true;
+//			Debug.Log("fuck you");
+			if (swapped == false)
+			{
+				yes = false;
+				no = true;
+			}
+			else
+			{
+				yes = true;
+				no = false;
+			}
 		}
 
 		
-		if (yes = true && Input.GetKeyUp(KeyCode.Space) && player.GetComponent<Timer>().timeUp == false && player.GetComponent<Timer>().timeLeft > 0)
+		if (yes == true && Input.GetKeyUp(KeyCode.Space) && player.GetComponent<Timer>().timeUp == false && player.GetComponent<Timer>().timeLeft > 0)
 		{
 			option1.GetComponent<Renderer>().enabled = false;
 			option2.GetComponent<Renderer>().enabled = false;
@@ -53,18 +76,49 @@ public class MenuSelection : MonoBehaviour
 			text2.SetActive(false);
 			answer.GetComponent<TextMesh>().text = "Yes.";
 			response.GetComponent<TextMesh>().text = "Haha fuck you.";
+			player.GetComponent<Timer>().timeLeft = 0;
 			player.GetComponent<Timer>().timeDisplay.GetComponent<TextMesh>().text = "You failed!";
 		}
 		
-		if (no = true && Input.GetKeyUp(KeyCode.Space) && player.GetComponent<Timer>().timeUp == false && player.GetComponent<Timer>().timeLeft > 0)
+		if (no == true && Input.GetKeyUp(KeyCode.Space) && player.GetComponent<Timer>().timeUp == false && player.GetComponent<Timer>().timeLeft > 0)
 		{
-			option1.GetComponent<Renderer>().enabled = false;
-			option2.GetComponent<Renderer>().enabled = false;
-			text1.SetActive(false);
-			text2.SetActive(false);
+			float red = Random.Range(0, 1f);
+			float green = Random.Range(0, 1f);
+			float blue = Random.Range(0, 1f);
+			int changeOptions = Random.Range(0, 2);
+			
+//			option1.GetComponent<Renderer>().enabled = false;
+//			option2.GetComponent<Renderer>().enabled = false;
+//			text1.SetActive(false);
+//			text2.SetActive(false);
 			answer.GetComponent<TextMesh>().text = "No.";
-			response.GetComponent<TextMesh>().text = " ";
+			answer.GetComponent<TextMesh>().color = new Color(red, green, blue);
+			response.GetComponent<TextMesh>().text = "Please?";
+			response.GetComponent<TextMesh>().characterSize += .01f;
 			player.GetComponent<Timer>().timeDisplay.GetComponent<TextMesh>().text = "You did it!";
+			player.GetComponent<Timer>().timeLeft += timeAdd;
+			if (timeAdd > .5f)
+			{
+				timeAdd -= .5f;
+			}
+
+			if (changeOptions == 0)
+			{
+				text1.GetComponent<TextMesh>().text = "Yes";
+				text2.GetComponent<TextMesh>().text = "No";
+				swapped = false;
+			}
+			else
+			{
+				text1.GetComponent<TextMesh>().text = "No";
+				text2.GetComponent<TextMesh>().text = "Yes";
+				swapped = true;
+			}
+			
+			option1.GetComponent<Renderer>().enabled = true;
+			option2.GetComponent<Renderer>().enabled = true;
+			yes = false;
+			no = false;
 		}
 	}
 }
