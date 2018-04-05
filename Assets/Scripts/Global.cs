fe6e29c6029ad8ef9504e586d36b6010dd173f59
John <jw4429@nyu.edu>
2018-04-05 11:04:42 -0400

    Added the end game scene, and the global script

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviourSingleton<Global> //acts almost like a bank for the global variables to be called later
+{
+	public static Global me;
+
+	public int score; 
+
+	private void Awake()
+	{
+		me = this; //awakens the script
+	}
+}
